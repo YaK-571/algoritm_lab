0698fa70005a413d460c8a94c128fc9fda6e98e2
YaK-571 <65449904+YaK-571@users.noreply.github.com>
2022-12-22 01:11:46 +0300

    Add files via upload

---
+++ lab_3.2/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,88 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_3.2/Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		//проверка работает ли сортировка вставками на частном примере
+		TEST_METHOD(TestMethod1)
+		{
+				int a[10]{ 8,12,6, 1, 17, 18, 200, 400, 11, 5 };
+
+				insertionSort(a, a + 10 - 1, [](int a, int b) {return a < b; });
+
+				Assert::IsTrue(a[2] == 6);
+		}
+
+		//проверка работает ли быстрая сортировка на частном примере
+		TEST_METHOD(TestMethod2)
+		{
+
+			int a[10]{ 1,30,60,40,12,500,21,40,123,499 };
+
+			quickSort(a, a + 10 - 1, [](int a, int b) {return a < b; });
+
+			Assert::IsTrue(a[9] == 500);
+		}
+
+		//проверка совпадает ли результат у разных сортировок
+		TEST_METHOD(TestMethod3)
+		{
+
+			int a[10]{ 10,9,8, 7, 6, 5, 4, 3, 2, 1 };
+			int b[10]{ 10,9,8, 7, 6, 5, 4, 3, 2, 1 };
+
+			insertionSort(a, a + 10 - 1, [](int a, int b) {return a < b; });
+			quickSort(b, b + 10 - 1, [](int a, int b) {return a < b; });
+
+			Assert::IsTrue(a[0] == b[0]);
+			Assert::IsTrue(a[1] == b[1]);
+			Assert::IsTrue(a[2] == b[2]);
+			Assert::IsTrue(a[3] == b[3]);
+			Assert::IsTrue(a[4] == b[4]);
+			Assert::IsTrue(a[5] == b[5]);
+			Assert::IsTrue(a[6] == b[6]);
+			Assert::IsTrue(a[7] == b[7]);
+			Assert::IsTrue(a[8] == b[8]);
+			Assert::IsTrue(a[9] == b[9]);
+		}
+
+		//проверка того, что элементы действительн располагаются в нужном порядке
+		TEST_METHOD(TestMethod4)
+		{
+
+			int a[10]{ 8,12,6, 1, 17, 4, 3, 400, 11, 2 };
+
+			sort(a, a + 10 - 1, [](int a, int b) {return a < b; });
+
+			Assert::IsTrue(a[0] == 1);
+			Assert::IsTrue(a[1] == 2);
+			Assert::IsTrue(a[2] == 3);
+			Assert::IsTrue(a[3] == 4);
+		}
+
+		//проверка, что каждый элемент после сортировки не больше последующего
+		TEST_METHOD(TestMethod5)
+		{
+
+			int a [10]{ 8,12,6, 1, 17, 18, 200, 400, 11, 5 };
+
+			sort(a, a + 10 - 1, [](int a, int b) {return a < b; });
+
+			int i = 0;
+			while (i < 9)
+			{
+				Assert::IsTrue(a[i] <= a[i+1]);
+				i = i + 1;
+			}
+		}
+
+		
+	};
+}
---
+++ lab_3.2/lab_3.2/lab_3.2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <fstream>
+#include  "Header.h"
+
+int Random(int min, int max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(min, max);
+    return dist(gen);
+}
+
+template<typename T>
+
+void Save(int a, T b, T c)
+{
+    std::ofstream outData;
+    outData.open("Save.csv", std::ios::app);
+    outData << a <<";" << b <<";" << c << std::endl;
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    //создание массивов разной длины, для проверки скорости сортировок
+    for (int size = 5; size <= 100; size++) {
+
+        std::chrono::nanoseconds insertTime = std::chrono::nanoseconds(0);
+        std::chrono::nanoseconds quickSortTime = std::chrono::nanoseconds(0);
+
+        int* arrInsert = new int[size];
+        int* arrQuickSort = new int[size];
+
+        for (int j = 0; j < 100; j++) {
+            //создание двух рандомных массивов. Первый для сортировки вставками, второй для быстрой сортировки
+            for (int i = size - 1, k = 1; i >= 0; i--, k++) {
+                arrInsert[i] = k;
+            }
+
+            for (int i = 0; i < size; i++) {
+                arrQuickSort[i] = Random(1, 100);
+            }
+
+
+            //таймер и сортировка
+            auto start = std::chrono::high_resolution_clock::now();
+            insertionSort(arrInsert, arrInsert + size - 1, [](int a, int b) {return a < b; });
+            auto end = std::chrono::high_resolution_clock::now();
+            insertTime = (end - start);
+
+            //таймер и сортировка
+            start = std::chrono::high_resolution_clock::now();
+            quickSort(arrQuickSort, arrQuickSort + size - 1, [](int a, int b) {return a < b; });
+            end = std::chrono::high_resolution_clock::now();
+            quickSortTime = (end - start);
+
+
+        }
+       
+        Save(size, insertTime.count(), quickSortTime.count());
+       
+        //очистка массивов
+        delete[] arrInsert;
+        delete[] arrQuickSort;
+
+    }
+
+}
